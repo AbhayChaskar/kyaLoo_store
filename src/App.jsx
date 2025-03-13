387e76185e3c137900e9d162c8d0b132b9c5f75b
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-13 19:40:10 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Header from './Components/Header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductComponent from './Components/ProductComponent'
+import ProductDetails from './Components/ProductDetails'
 
 function App() {
   return (
-      <div className='App'>
+    <Router>
+    <div className='App'>
         <Header />
-      </div>
+        <Routes>
+          <Route path='/' element={<ProductComponent />} />
+          <Route path='/product/:productId' element={<ProductDetails />} />
+        </Routes>
+    </div>
+    </Router>
   )
 }
 
