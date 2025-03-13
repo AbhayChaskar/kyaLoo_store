b5761470bb0a9bc5f053fc9bdad2baa33b8e547d
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-13 19:25:26 +0530

    Navbar styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import "./Header.css"
 
 const Header = () => {
   return (
-    <div className="navbar">
-      <div className="">
-        <h3>KyaLoo Store</h3>
-      </div>
+    <nav class="navbar">
+    <h3 class="navbar-logo">KyaLoo Store</h3>
+    <div class="navbar-links">
+        <a href="#">Home</a>    
     </div>
+    <button class="navbar-button">Explore</button>
+</nav>
   );
 };
 
